3755579e17286e1983c2b80697c475ebf399e6c6
Lucio Gutierrez <lucio.gutierrez30@yahoo.com>
2017-01-19 10:06:39 -0600

    imported validator

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Validator from 'validator'
 
 export default React.createClass({
   render() {
     return(
       <section>
+        <form>
+          <input type="email"></input>
+          <input type="submit"></input>
+        </form>
       </section>
     )
   }
