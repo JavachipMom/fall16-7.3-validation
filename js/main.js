8eb842101c71a3ec8ba6ff2ead4cc22aa865a3fb
David Rangel <rangelography@gmail.com>
2017-01-19 11:56:10 -0600

    setState for email input and validated with turnary to change classes in input form

--- js/main.js
+++ js/main.js
@@ -4,18 +4,19 @@ import Validator from 'validator'
 export default React.createClass({
   getInitialState(){
     return{
-      emailText: ""
+      isEmailValid: false
     }
   },
   onEmailChange(e){
     // Set the state of emailText to value of email input
     // var emailInputValue =
-    console.log(e.target.value);
+    var isEmail = Validator.isEmail(e.target.value);
+    this.setState({isEmailValid:isEmail})
   },
   render() {
     return(
       <section>
-        <form>
+        <form className={this.state.isEmailValid ? "valid" : "invalid"}>
           <input type="email" onChange={this.onEmailChange} />
           <input type="submit"/>
         </form>
