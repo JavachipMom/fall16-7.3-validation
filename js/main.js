d9d08f05398ebcc6632a1e55cefe074c86f52ca8
Nicolas Roybal <nicolas.roybal@hotmail.com>
2017-01-19 11:09:25 -0600

    Added to content to main.js

--- js/main.js
+++ js/main.js
@@ -2,12 +2,22 @@ import React from 'react'
 import Validator from 'validator'
 
 export default React.createClass({
+  getInitialState(){
+    return{
+      emailText: ""
+    }
+  },
+  onEmailChange(e){
+    // Set the state of emailText to value of email input
+    // var emailInputValue =
+    console.log(e.target.value);
+  },
   render() {
     return(
       <section>
         <form>
-          <input type="email"></input>
-          <input type="submit"></input>
+          <input type="email" onChange={this.onEmailChange} />
+          <input type="submit"/>
         </form>
       </section>
     )
