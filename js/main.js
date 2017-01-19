7afbc2f4412b37a4d18a15b767f16fff823ab295
JavachipMom <Nicolesp4ever@gmail.com>
2017-01-19 13:43:21 -0600

    Started the lecture for 7.4-stored data

--- js/main.js
+++ js/main.js
@@ -1,11 +1,28 @@
 import React from 'react'
+import { ajax } from 'jquery'
 import Validator from 'validator'
 
 export default React.createClass({
+  componentDidMount() {
+    // This component has been loaded into memory
+    ajax({
+      url: "https://tiny-tiny.herokuapp.com/collections/fall16",
+      dataType: "json",
+      success: this.onAjaxLoadSuccess,
+      error: this.onAjaxLoadError
+    })
+  },
   getInitialState(){
+    // url: `https://tiny-tiny.herokuapp.com/collections/sa_tech_userdata_test3/${this.props.params.userID}`
     return{
       isEmailValid: false
     }
+  },
+  onAjaxLoadSuccess(response){
+    console.log(response);
+  },
+  onAjaxLoadError(response){
+
   },
   onEmailChange(e){
     // Set the state of emailText to value of email input
